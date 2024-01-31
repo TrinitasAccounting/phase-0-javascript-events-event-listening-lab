258cc98dfac394e207cc4c41c58bf5b5b037317a
Clay Mangum <clay@trinitasaccounting.com>
2024-01-31 08:46:16 -0600

    complete

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-function addingEventListener() {
-    const input = document.getElementById('button');
 
-    function clickAlert() {
-        alert('I was clicked!');
-    }
 
-    input.addEventListener('click', clickAlert);
-};
+const addingEventListener = () => {
+    const button = document.getElementById('button');
+
+    button.addEventListener('click', () => {
+        return alert('Hi I was clicked');
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addingEventListener() {
+//     const input = document.getElementById('button');
+
+//     function clickAlert() {
+//         alert('I was clicked!');
+//     }
+
+//     input.addEventListener('click', clickAlert);
+// };
 
 
